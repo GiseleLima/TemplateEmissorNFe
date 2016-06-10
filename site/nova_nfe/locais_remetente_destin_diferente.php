b69ac2bb1f1f64ca133c1ec50685e2ce3e7df861
Gisele Lima <gislef@hotmail.com>
2016-06-10 13:09:46 -0300

    Identação
    Correção

--- site/nova_nfe/locais_remetente_destin_diferente.php
+++ site/nova_nfe/locais_remetente_destin_diferente.php
@@ -2,144 +2,144 @@
  não sendo necessarias diversas concatenações no codigo, facilitando visualmente para adicionar PHP-->
  <script id="retiradaDif">
  <div class="jumbotron">
-													<div class="container">
-													<div class="col-md-12">
-														<label for="ex2">Tipo Documento</label>
-														<select class="form-control" name="tipo-doc-retirada-dif-emitente">
-																<option>CNPJ</option>
-																<option>CPF</option>
-															</select>
-														</div>			
-														<div class="col-md-12">
-																<label for="ex2">CNPJ/CPF</label>
-																<input name="cnpj-cpf-retirada-dif-emitente" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-12">
-																<label for="ex2">Logadouro</label>
-																<input name="logadouro-retirada-dif-emitente" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-4">
-																<label for="ex2">Numero</label>
-																<input name="numero-retirada-dif-emitente" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-8">
-																<label for="ex2">Complemento</label>
-																<input name="complemento-retirada-dif-emitente" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-12">
-																<label for="ex2">Bairro</label>
-																<input name="bairro-retirada-dif-emitente" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-4">
-															 <label for="ex2">UF</label>
-																<select class="form-control">
-																  <option>AC</option>
-																  <option>AL</option>
-																  <option>AP</option>
-																  <option>AM</option>
-																  <option>BA</option>
-																  <option>CE</option>
-																  <option>DF</option>
-																  <option>ES</option>
-																  <option>GO</option>
-																  <option>MA</option>
-																  <option>MT</option>
-																  <option>MS</option>
-																  <option>MG</option>
-																  <option>PA</option>
-																  <option>PB</option>
-																  <option>PA</option>
-																  <option>PR</option>
-																  <option>PE</option>
-																  <option>PI</option>
-																  <option>RJ</option>
-																  <option>RN</option>
-																  <option>RO</option>
-																  <option>RR</option>
-																  <option>SC</option>
-																  <option>SP</option>
-																  <option>TO</option>
-																  <option>EX</option>													  
-																</select>
-														</div>
-														<div class="col-md-8">
-																<label for="ex2">Municipio</label>
-																<input name="municipio-retirada-dif-emitente" class="form-control" id="ex2" type="text">
-														</div>
-														
-													</div>
-											   </div>
+	<div class="container">
+	<div class="col-md-12">
+		<label for="ex2">Tipo Documento</label>
+		<select class="form-control" name="tipo-doc-retirada-dif-emitente">
+				<option>CNPJ</option>
+				<option>CPF</option>
+			</select>
+		</div>			
+		<div class="col-md-12">
+				<label for="ex2">CNPJ/CPF</label>
+				<input name="cnpj-cpf-retirada-dif-emitente" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-12">
+				<label for="ex2">Logadouro</label>
+				<input name="logadouro-retirada-dif-emitente" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-4">
+				<label for="ex2">Numero</label>
+				<input name="numero-retirada-dif-emitente" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-8">
+				<label for="ex2">Complemento</label>
+				<input name="complemento-retirada-dif-emitente" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-12">
+				<label for="ex2">Bairro</label>
+				<input name="bairro-retirada-dif-emitente" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-4">
+			 <label for="ex2">UF</label>
+				<select class="form-control">
+				  <option>AC</option>
+				  <option>AL</option>
+				  <option>AP</option>
+				  <option>AM</option>
+				  <option>BA</option>
+				  <option>CE</option>
+				  <option>DF</option>
+				  <option>ES</option>
+				  <option>GO</option>
+				  <option>MA</option>
+				  <option>MT</option>
+				  <option>MS</option>
+				  <option>MG</option>
+				  <option>PA</option>
+				  <option>PB</option>
+				  <option>PA</option>
+				  <option>PR</option>
+				  <option>PE</option>
+				  <option>PI</option>
+				  <option>RJ</option>
+				  <option>RN</option>
+				  <option>RO</option>
+				  <option>RR</option>
+				  <option>SC</option>
+				  <option>SP</option>
+				  <option>TO</option>
+				  <option>EX</option>													  
+				</select>
+		</div>
+		<div class="col-md-8">
+				<label for="ex2">Municipio</label>
+				<input name="municipio-retirada-dif-emitente" class="form-control" id="ex2" type="text">
+		</div>
+		
+	</div>
+</div>
 </script>
 
 
  <script id="entregaDif">
  
  <div class="jumbotron" id="entrega-diferente-destinatario">
-													<div class="container">
-													<div class="col-md-12">
-														<label for="ex2">Tipo Documento</label>
-														<select class="form-control" name="tipo-doc-entrega-dif-destinatario">
-																<option>CNPJ</option>
-																<option>CPF</option>
-															</select>
-														</div>			
-														<div class="col-md-12">
-																<label for="ex2">CNPJ/CPF</label>
-																<input name="cnpj-cpf-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-12">
-																<label for="ex2">Logadouro</label>
-																<input name="logadouro-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-4">
-																<label for="ex2">Numero</label>
-																<input name="numero-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-8">
-																<label for="ex2">Complemento</label>
-																<input name="complemento-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-12">
-																<label for="ex2">Bairro</label>
-																<input name="bairro-rentrega-dif-destinatario" class="form-control" id="ex2" type="text">
-														</div>
-														<div class="col-md-4">
-															 <label for="ex2">UF</label>
-																<select class="form-control">
-																  <option>AC</option>
-																  <option>AL</option>
-																  <option>AP</option>
-																  <option>AM</option>
-																  <option>BA</option>
-																  <option>CE</option>
-																  <option>DF</option>
-																  <option>ES</option>
-																  <option>GO</option>
-																  <option>MA</option>
-																  <option>MT</option>
-																  <option>MS</option>
-																  <option>MG</option>
-																  <option>PA</option>
-																  <option>PB</option>
-																  <option>PA</option>
-																  <option>PR</option>
-																  <option>PE</option>
-																  <option>PI</option>
-																  <option>RJ</option>
-																  <option>RN</option>
-																  <option>RO</option>
-																  <option>RR</option>
-																  <option>SC</option>
-																  <option>SP</option>
-																  <option>TO</option>
-																  <option>EX</option>													  
-																</select>
-														</div>
-														<div class="col-md-8">
-																<label for="ex2">Municipio</label>
-																<input name="municipio-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
-														</div>
-														
-													</div>
-											   </div>	 
+	<div class="container">
+	<div class="col-md-12">
+		<label for="ex2">Tipo Documento</label>
+		<select class="form-control" name="tipo-doc-entrega-dif-destinatario">
+				<option>CNPJ</option>
+				<option>CPF</option>
+			</select>
+		</div>			
+		<div class="col-md-12">
+				<label for="ex2">CNPJ/CPF</label>
+				<input name="cnpj-cpf-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-12">
+				<label for="ex2">Logadouro</label>
+				<input name="logadouro-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-4">
+				<label for="ex2">Numero</label>
+				<input name="numero-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-8">
+				<label for="ex2">Complemento</label>
+				<input name="complemento-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-12">
+				<label for="ex2">Bairro</label>
+				<input name="bairro-rentrega-dif-destinatario" class="form-control" id="ex2" type="text">
+		</div>
+		<div class="col-md-4">
+			 <label for="ex2">UF</label>
+				<select class="form-control">
+				  <option>AC</option>
+				  <option>AL</option>
+				  <option>AP</option>
+				  <option>AM</option>
+				  <option>BA</option>
+				  <option>CE</option>
+				  <option>DF</option>
+				  <option>ES</option>
+				  <option>GO</option>
+				  <option>MA</option>
+				  <option>MT</option>
+				  <option>MS</option>
+				  <option>MG</option>
+				  <option>PA</option>
+				  <option>PB</option>
+				  <option>PA</option>
+				  <option>PR</option>
+				  <option>PE</option>
+				  <option>PI</option>
+				  <option>RJ</option>
+				  <option>RN</option>
+				  <option>RO</option>
+				  <option>RR</option>
+				  <option>SC</option>
+				  <option>SP</option>
+				  <option>TO</option>
+				  <option>EX</option>													  
+				</select>
+		</div>
+		<div class="col-md-8">
+				<label for="ex2">Municipio</label>
+				<input name="municipio-entrega-dif-destinatario" class="form-control" id="ex2" type="text">
+		</div>
+		
+	</div>
+</div>	 
 </script>
